2e514c279a3e4c4ebf9ac3c79fa0279849a5fd58
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-12-10 20:30:39 +0100

    remove most of the utils functions and rename file to keymap

---
+++ src/utils/keymap.js
@@ -0,0 +1,16 @@
+export function defaultKeyMap() {
+  return {
+    ArrowUp: "up",
+    ArrowDown: "down",
+    ArrowLeft: "left",
+    ArrowRight: "right",
+    w: "up",
+    s: "down",
+    a: "left",
+    d: "right",
+    W: "up",
+    S: "down",
+    A: "left",
+    D: "right"
+  };
+}

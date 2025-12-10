0d5aaa8a45abfb94e85303984fb6c4b9b1849863
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-12-10 20:33:22 +0100

    classes to handle pickups and projectiles

---
+++ src/components/Pickup.js
@@ -0,0 +1,41 @@
+import { Vector } from "../utils/Vector.js";
+
+export class Pickup {
+    constructor(position, type = "food") {
+        this.position = position instanceof Vector ? position : new Vector(position.x, position.y);
+        this.type = type;
+        this.id = Math.random().toString(36).slice(2, 8);
+    }
+
+    isAt(pos) {
+        return this.position.equals(pos);
+    }
+
+    applyTo(snake) {
+        switch (this.type) {
+            case "food":
+                snake.grow(1);
+                break;
+            case "ammo":
+                snake.ammo += 1;
+                break;
+            case "armor":
+                snake.armor += 1;
+                break;
+        }
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            position: this.position.toJSON(),
+            type: this.type
+        };
+    }
+
+    static fromJSON(data) {
+        const pickup = new Pickup(Vector.fromJSON(data.position), data.type);
+        pickup.id = data.id;
+        return pickup;
+    }
+}
